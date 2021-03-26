cad40cc73de216275ddb6c01716523f6d3ad78c5
qqbbppdd <76839089+qqbbppdd@users.noreply.github.com>
2021-03-26 14:43:55 +0300

    Add files via upload

--- wtf-code/projects.js
+++ wtf-code/projects.js
@@ -5,74 +5,9 @@ const Projects = [
         icon_path:"WebCreate/logo.png"
     },
     {
-        name:"Игра unturned",
-        path:"WebCreate/index.html",
-        icon_path:"https://i0.wp.com/www.hisour.com/wp-content/uploads/2018/01/Landscape-painting.jpg?fit=960%2C640&ssl=1"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"visual studio",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
-    {
-        name:"Игра майнкрафт",
-        path:"WebCreate/index.html",
-        icon_path:"NONI"
-    },
+        name:"Pablo Бот",
+        path:"Pablo/index.html",
+        icon_path:"Pablo/logo.png"
+    }
 ];
 
