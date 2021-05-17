4003e02c405d9b18ae22ea3d5ca419880375dd9f
qqbbppdd <76839089+qqbbppdd@users.noreply.github.com>
2021-05-17 17:55:27 +0300

    Add files via upload

--- trash/quartz/qscript.js
+++ trash/quartz/qscript.js
@@ -1,3 +1,4 @@
+var menu = document.getElementsByClassName("qmenu")[0];
 var logo = document.getElementsByClassName("qtextlogo")[0];
 var ul = document.getElementById("ul");
 var mbutton = document.getElementsByClassName("qmenubutton")[0];
@@ -18,6 +19,7 @@ function MenuFromCompact() {
     if(!compact_menu) return;
     compact_menu = false;
     menu_shown = false;
+    menu.style.borderBottomRightRadius = "20px";
     ul.style.animation = "none";
     ul.classList.remove("compacted");
     mbutton.style.visibility = "hidden";
@@ -34,10 +36,12 @@ function Update() {
 mbutton.addEventListener("click",function(ev) {
     if(menu_shown) {
         ul.style.animation = "sidebar-close 0.4s both";
+        menu.style.borderBottomRightRadius = "20px";
         menu_shown = false;
     }
     else {
         ul.style.animation = "sidebar-open 0.4s both";
+        menu.style.borderBottomRightRadius = "0";
         menu_shown = true;
     }
 });
