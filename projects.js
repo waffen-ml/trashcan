6e0b14fa7b9044e945460f86cbe62e22858e6726
qqbbppdd <76839089+qqbbppdd@users.noreply.github.com>
2021-05-18 23:35:47 +0300

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,13 @@
+const Projects = [
+    {
+        name:"WebCreate Редактор",
+        path:"WebCreate/index.html",
+        icon_path:"WebCreate/logo.png"
+    },
+    {
+        name:"Pablo Бот",
+        path:"Pablo/index.html",
+        icon_path:"Pablo/logo.png"
+    }
+];
+
